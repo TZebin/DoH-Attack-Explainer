d1bd84d7fee9f7ad31c1e3712c25cffcdf260a4e
Shahadate Rezvy <53818028+Shahadate-Rezvy@users.noreply.github.com>
2021-07-20 02:22:35 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,8 +19,8 @@ pkl_dir = Path.cwd() / "pkls"
 
 app = Flask(__name__)
 
-class_explainer = ClassifierExplainer.from_file(pkl_dir / "class_explainer.joblib")
-class_dashboard = ExplainerDashboard(class_explainer, 
+class_explainer = ClassifierExplainer.from_file(pkl_dir / "clas_explainer.joblib")
+class_dashboard = ExplainerDashboard(clas_explainer, 
                     title="Classifier Explainer: Predicting Health_status for Salmon", 
                     server=app, url_base_pathname="/classifier/", 
                     header_hide_selector=True)
@@ -33,7 +33,8 @@ index_app = dash.Dash(
     external_stylesheets=[BOOTSTRAP])
 
 index_app.title = 'Salmon explainerdashboard'
-
+index_app.layout = index_layout
+register_callbacks(index_app)
 
 @app.route("/")
 def index():
@@ -42,9 +43,3 @@ def index():
 @app.route('/classifier')
 def classifier_dashboard():
     return class_dashboard.app.index()
-
-
-
-
-
-    
