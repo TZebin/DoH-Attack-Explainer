7d4073fcc86110452e5255247a435e6e1a96ff6b
Shahadate Rezvy <53818028+Shahadate-Rezvy@users.noreply.github.com>
2021-07-20 01:02:50 +0100

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,51 @@
+
+# xgboost is a dependency of dtreeviz, but too large (>350M) for heroku
+# so we uninstall it and mock it here:
+from unittest.mock import MagicMock
+import sys
+sys.modules["xgboost"] = MagicMock()
+
+from pathlib import Path
+from flask import Flask
+
+import dash
+from dash_bootstrap_components.themes import FLATLY, BOOTSTRAP # bootstrap theme
+from explainerdashboard import *
+
+from index_layout import index_layout, register_callbacks
+from custom import CustomModelTab, CustomPredictionsTab
+
+pkl_dir = Path.cwd() / "pkls"
+
+app = Flask(__name__)
+
+clas_explainer = ClassifierExplainer.from_file(pkl_dir / "class_explainer.joblib")
+clas_dashboard = ExplainerDashboard(clas_explainer, 
+                    title="Classifier Explainer: Predicting Health_status for Salmon", 
+                    server=app, url_base_pathname="/classifier/", 
+                    header_hide_selector=True)
+
+
+index_app = dash.Dash(
+    __name__, 
+    server=app, 
+    url_base_pathname="/", 
+    external_stylesheets=[BOOTSTRAP])
+
+index_app.title = 'Salmon explainerdashboard'
+index_app.layout = index_layout
+register_callbacks(index_app)
+
+@app.route("/")
+def index():
+    return index_app.index()
+
+@app.route('/classifier')
+def classifier_dashboard():
+    return clas_dashboard.app.index()
+
+
+
+
+
+    
---
+++ generate_explainers.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from explainerdashboard import ClassifierExplainer, ExplainerDashboard
+
+
+pkl_dir = Path.cwd() / "pkls"
+data_dir= Path.cwd() / "data"
+df=pd.read_csv(data_dir /'new_processed.csv')
+y=df['JB_category']
+X=df.drop(columns=['JB_category'])
+
+
+
+# classifier
+X_train, X_test,y_train, y_test=train_test_split(X,y, stratify=y,test_size = 0.1, random_state=42)
+# classifier
+model = RandomForestClassifier(n_estimators=10, random_state=42,max_depth=5,class_weight='balanced')
+model.fit(X_train, y_train)
+class_explainer = ClassifierExplainer(model, X_test, y_test, 
+                               labels=['HEALTHY', 'UNHEALTHY'])
+_ = ExplainerDashboard(class_explainer)
+class_explainer.dump(pkl_dir/ "class_explainer.joblib")
