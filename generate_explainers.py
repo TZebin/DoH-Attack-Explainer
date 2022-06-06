7903e1ca7831dbdae013d9c73cacec1e43755bd4
TZebin <31511385+TZebin@users.noreply.github.com>
2022-06-06 19:50:48 +0100

    Update generate_explainers.py

--- generate_explainers.py
+++ generate_explainers.py
@@ -20,7 +20,7 @@ X_train, X_test,y_train, y_test=train_test_split(X,y, stratify=y,test_size = 0.1
 model = RandomForestClassifier(n_estimators=10, random_state=42,max_depth=5,class_weight='balanced')
 model.fit(X_train, y_train)
 class_explainer = ClassifierExplainer(model, X_test, y_test, 
-                               labels=['HEALTHY', 'UNHEALTHY'])
+                               labels=['Non-DOH', 'Benign-DoH', 'Malicious-DOH'])
 _ = ExplainerDashboard(class_explainer)
 #class_explainer.dump(pkl_dir/ "class_explainer.joblib")
 class_explainer.dump(pkl_dir/ "explainer.pkl")
